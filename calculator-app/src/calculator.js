a33bd035cf863579b3fd036903c6d78eb1f6c013
Raja <rajalingam210901@gmail.com>
2023-10-20 22:01:44 +0530

    Added the Buttons

---
+++ calculator-app/src/calculator.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./calculator.css";
+class Calculator extends React.Component{
+render(){
+return<div className="calulatorContainer">
+
+   <span>00</span>
+
+   <div className="Numbers">
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      <button>1</button>
+      
+
+   </div>
+    
+   </div>
+}
+
+}
+
+export default Calculator;
