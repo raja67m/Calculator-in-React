5cf30fb99b98a63e6c247270f193be3d6787a6c5
Raja <rajalingam210901@gmail.com>
2023-10-22 11:35:02 +0530

    Styles added

--- calculator-app/src/calculator.js
+++ calculator-app/src/calculator.js
@@ -2,30 +2,39 @@ import React from "react";
 import "./calculator.css";
 class Calculator extends React.Component{
 render(){
-return<div className="calulatorContainer">
+return<>
+<h1>Calculator</h1>
 
-   <span>00</span>
+<div className="calulatorContainer">
 
-   <div className="Numbers">
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
-      <button>1</button>
+
+   
+<span>00</span>
+   <div className="calNumbers">
+   
+     
+<button className="Numbers">7</button>
+      <button className="Numbers">8</button>
+      <button className="Numbers">9</button>
+      <button className="symbols">x</button>
+      <button className="Numbers">4</button>
+      <button className="Numbers">5</button>
+      <button className="Numbers">6</button>
+      <button className="symbols">-</button>
+      <button className="Numbers">1</button>
+      <button className="Numbers">2</button>
+      <button className="Numbers">3</button>
+      <button className="symbols">+</button>
+      <button className="Numbers">0</button>
+      <button className="symbols">c</button>
+      <button className="symbols">=</button>
+      <button className="symbols">/</button>
       
 
    </div>
     
    </div>
+   </>
 }
 
 }
